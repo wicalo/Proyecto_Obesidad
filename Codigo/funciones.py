438dd418110a6b25adade96be5a52995853a0fbb
wocalo <wicaloe@gmail.com>
2024-03-25 12:46:03 -0500

    [RL20]

--- Codigo/funciones.py
+++ Codigo/funciones.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.inspection import permutation_importance
 
 def redondear_variables(df, variables):
     for var in variables:
@@ -7,3 +8,11 @@ def redondear_variables(df, variables):
     return df
 
 
+def importancia(flujo,X,Y ):
+    importancia_modelo = permutation_importance(flujo,X,Y, n_repeats=10, random_state=0)
+    ordenado = importancia_modelo.importances_mean.argsort()
+    importancias = pd.DataFrame(
+    importancia_modelo.importances[ordenado].T,
+    columns=X.columns[ordenado])
+   
+    return importancias
